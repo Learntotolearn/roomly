97fbe1a42ef6ee5f7a0a0d903a0418a65d326339
BIGW\13007 <1300775666@qq.com>
2025-09-17 23:26:21 +0800

    refactor: 将next.config.js的配置迁移至next.config.ts
    删除了旧的next.config.js文件，并将相关配置整合到next.config.ts中，确保项目配置统一性。

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
